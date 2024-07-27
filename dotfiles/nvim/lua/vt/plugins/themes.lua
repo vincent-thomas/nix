158148ff92c4229166c18565ddd499e048845366
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2024-07-27 23:41:07 +0200

    catppuccin neovim

--- dotfiles/nvim/lua/vt/plugins/themes.lua
+++ dotfiles/nvim/lua/vt/plugins/themes.lua
@@ -2,8 +2,16 @@ return {
   {
     "olimorris/onedarkpro.nvim",
     priority = 10000,
+    -- config = function()
+    --   vim.cmd("colorscheme onedark")
+    -- end
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     config = function()
-      vim.cmd("colorscheme onedark")
-    end
+      vim.cmd("colorscheme catppuccin-frappe")
+    end,
   },
 }

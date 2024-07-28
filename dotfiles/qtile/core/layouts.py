c4a8a38712457af10506ecc032974eb7c782eba7
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2024-07-28 14:07:21 +0200

    testing qtile layouts

--- dotfiles/qtile/core/layouts.py
+++ dotfiles/qtile/core/layouts.py
@@ -2,6 +2,17 @@ from libqtile import layout
 from core.theme import theme
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], **theme),
-    layout.Max(**theme),
+        layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Max(),
+    # Try more layouts by unleashing below layouts.
+    layout.Stack(num_stacks=2),
+    layout.Bsp(),
+    layout.Matrix(),
+    layout.MonadTall(),
+    layout.MonadWide(),
+    layout.RatioTile(),
+    layout.Tile(),
+    layout.TreeTab(),
+    layout.VerticalTile(),
+    layout.Zoomy(),
 ]
